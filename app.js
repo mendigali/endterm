73425a8838eec9e7ce08cc2218df21cc05e391b4
Mendigali Temir <51957278+mendigali@users.noreply.github.com>
2021-03-13 08:27:58 +0600

    Switched port from 80 to 443

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const fs = require('fs');
 const app = express();
-const port = 80;
+const port = 443;
 
 app.set('view engine', 'ejs');
 
