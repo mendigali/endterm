3c32f1fb9ff3e691a9f53d96b0e53649400670c1
Mendigali Temir <51957278+mendigali@users.noreply.github.com>
2021-03-13 08:24:45 +0600

    Changed port from 3000 to 80

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.set('view engine', 'ejs');
 
