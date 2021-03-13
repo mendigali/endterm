4fec5a24332acab64c1e3209ac17d0357942a2b3
Mendigali Temir <51957278+mendigali@users.noreply.github.com>
2021-03-13 08:32:30 +0600

    Added .env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const fs = require('fs');
 const app = express();
-const port = 443;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
